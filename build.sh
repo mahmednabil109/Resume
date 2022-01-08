3c78a13bddf213281393a85633c0f4aaafbe6796
mahmednabil109 <mahmednabil109@gmail.com>
2022-01-09 01:02:30 +0200

    DEPLOY:Sun Jan  9 01:02:30 AM EET 2022

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 git checkout deploy
+<<<<<<< HEAD
 git rebase master
 cp Resume.html index.html
 git add .
 git commit -m "DEPLOY:$(date)"
 git push origin deploy
 git checkout master
+=======
+cp Resume.html index.html
+git add .
+git commit -m "DEPLOY:$(date)"
+>>>>>>> 8fd8ada (DEPLOY:Sun Jan  9 01:02:30 AM EET 2022)
